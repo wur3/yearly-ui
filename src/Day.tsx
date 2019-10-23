2a552393afa4daab940702c1eadf1342a95ff59a
wur3 <wu.richard.98@gmail.com>
2019-10-23 18:46:58 -0400

    satisfied with css i think

--- src/Day.tsx
+++ src/Day.tsx
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import './Day.css';
 
-
+var classNames = require('classnames');
 export class Day extends Component<{val: number},{}> {
+  state = {
+      'isSpecial': false
+  };
   render(){
+    var dayClass = classNames({
+      'Day': true,
+      // 'highlight': this.state.isSpecial
+      'highlight': (this.props.val===2)
+    });
     return (
-      <div className="Day">
+      <div className={dayClass}>
         {this.props.val}
       </div>
     );
